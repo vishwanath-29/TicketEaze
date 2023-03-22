106e438617c096032b316189242384aad8683706
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2023-03-23 00:38:15 +0530

    Added password rule (8 length , 1 special character , 1 capital , 1 number), show password option , client side validation

--- static/scripts/Register.js
+++ static/scripts/Register.js
@@ -15,4 +15,46 @@ checkbox.addEventListener("change",()=>{
     }
     
 })
+function show_password() {
+    var password1 = document.getElementsByName("password1")[0];
+    var password2 = document.getElementsByName("password2")[0];
+    if (password1.type === "password") {
+      password1.type = "text";
+      password2.type = "text";
+    } else {
+      password1.type = "password";
+      password2.type = "password";
+    }
+  }
+function submit_action(){
+    var passw = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+    var password1 = String(document.getElementsByName("password1")[0].value)
+    var password2 = String(document.getElementsByName("password2")[0].value)
+    if (password1==password2)
+        {
+            try{
+                console.log(password1+"   "+password2)
+                if (passw.test(password1)) 
+            {
+                console.log("Good ")
+                return false
+            }
+            else
+            {
+
+            alert("Passwords did not meet criteria:\n 1.Minimum of 8 characters \n 2.One Special Character \n 3.One Digit \n 4.One Capital Letter ")
+            return false
+            }
+
+            }
+            catch(e){
+                console.log(e)
+            }
+            
+          
+        }
+    else
+    alert("Passwords did not match!! ")
+    return false
+}
 
