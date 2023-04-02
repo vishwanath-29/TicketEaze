dc4840e15faf09467110f58a7468e86c5ed80d92
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2023-04-02 15:24:58 +0530

    Added Server Side Session Storage

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template , request , redirect ,session ,flash
 from flask_login import UserMixin,login_user,LoginManager,current_user,logout_user,login_required
 from flask_migrate import Migrate
+from flask_session import Session
 
 from models import *
 import hashlib,datetime
@@ -12,6 +13,10 @@ app.config['SECRET_KEY'] = 'secret-key'
 app.config['PERMANENT_SESSION_LIFETIME'] = 0
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
+
+
 login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.login_view = "login"
