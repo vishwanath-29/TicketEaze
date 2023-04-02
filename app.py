ba16db5757595f6440e04f6a4bf7dc75507db561
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2023-04-02 16:02:15 +0530

    Addded comments and beautified

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template , request , redirect ,session ,flash
-from flask_login import UserMixin,login_user,LoginManager,current_user,logout_user,login_required
+from flask_login import login_user,LoginManager,current_user,logout_user,login_required
 from flask_migrate import Migrate
 from flask_session import Session
 
+
 from models import *
+from datetime import timedelta
 import hashlib,datetime
+
 
 # Intialization and Configs 
 app = Flask(__name__)
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ticketeaze.db'
 app.config['SECRET_KEY'] = 'secret-key'
-app.config['PERMANENT_SESSION_LIFETIME'] = 0
+
+# Server Side Sessions
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-
 Session(app)
 
-
+# Login Manager Configs
 login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.login_view = "login"
@@ -26,7 +30,7 @@ migrate = Migrate()
 migrate.init_app(app, db)
 db.init_app(app)
 
-
+# Load user based of current user type 
 @login_manager.user_loader
 def load_user(user_id):
         if session['user_type']=='user':
@@ -35,18 +39,19 @@ def load_user(user_id):
           record = admin.query.get(int(user_id))
         return record
 
-
+# Before First Request Logging out users and 
 @app.before_first_request  
 def init():
+    session.clear()
     logout_user()
     db.create_all()
 
-#Home page and Route 
+# Home page and Route 
 @app.route("/")
-def hello():
+def home():
   return render_template('Home/Home.html')
 
-#Register Route and Method
+# Register Route and Method
 @app.route("/register",methods=['GET','POST'])
 def register():
   if request.method=='POST':
@@ -56,7 +61,9 @@ def register():
     dateofbirth=datetime.datetime.strptime(dateofbirth, "%Y-%m-%d").date()
     phonenumber=request.form.get("phonenumber")
     password=request.form.get("password1")
+    # Hashing password in SHA256
     password=hashlib.sha256(password.encode()).hexdigest()
+    # Check if email already exist
     if user.query.filter_by(email=email).first():
       return render_template("Register/Register.html",status=1)  
     else:
@@ -67,6 +74,7 @@ def register():
   else:
     return render_template("Register/Register.html")
 
+# User logging
 @app.route("/login/user",methods=['GET','POST'])
 def user_login():
   if request.method=='POST':
@@ -77,12 +85,14 @@ def user_login():
     
     if user_login:
             if(user_login.password==password):
+                # Setting the current session user type as user for logging in 
                 session['user_type'] = 'user'
                 login_user(user_login) 
                 return redirect("/")        
   else:
      return render_template("Register/UserLogin.html")
 
+# Admin Login
 @app.route("/login/admin",methods=['GET','POST'])
 def admin_login():
   if request.method=='POST':
@@ -92,44 +102,50 @@ def admin_login():
     admin_login = db.session.query(admin).filter_by(email=email).first()
     if admin_login:
             if(admin_login.password==password):
+                # Setting the user type as admin for logging in
                 session['user_type'] = 'admin'
                 login_user(admin_login) 
                 return redirect("/management")
   return render_template("Register/AdminLogin.html")
 
- 
+# Logout user
 @app.route('/logout')
 def logout():
     logout_user()
     if session.get('was_once_logged_in'):
         del session['was_once_logged_in']
+    # Flashing the message to template for alerting the success of logging out
     flash('Have successfully logged out!')
     return redirect('/')
 
 
 @app.route("/management")
-@login_required
+@admin_login_required
 def management():
   return render_template("Admin/Management.html")
 
   
 @app.route("/management/venue")
+@admin_login_required
 def managevenue():
   return render_template("Admin/ManageVenue.html")
 
   
 @app.route("/management/event")
+@admin_login_required
 @login_required
 def manageevent():
   return render_template("Admin/ManageEvent.html")
 
   
 @app.route("/management/event/add")
+@admin_login_required
 @login_required
 def addevent():
   return render_template("Admin/AddEvent.html")
 
 @app.route("/management/venue/add")
+@admin_login_required
 @login_required
 def addvenue():
   return render_template("Admin/AddVenue.html")
@@ -138,30 +154,33 @@ if __name__ == "__main__":
   app.run(debug=True)
 
 
-import os
-from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
+# import os
+# from flask import Flask, flash, request, redirect, url_for
+# from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+# UPLOAD_FOLDER = '/path/to/the/uploads'
+# ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app = Flask(__name__)
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+# def upload_file():
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         # if user does not select file, browser also
+#         # submit a empty part without filename
+#         if file.filename == '':
+#             flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+
 
 
