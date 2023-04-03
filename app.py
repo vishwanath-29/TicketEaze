9286537b5e54d235ef621bd2c5ff195373fbebe2
gkvishal7 <gkvishal7@gmail.com>
2023-04-04 03:02:39 +0530

    Removed ticket_booking

--- app.py
+++ app.py
@@ -262,6 +262,20 @@ def editevent():
 def editvenue():
   return render_template("Admin/EditVenue.html")
 
+<<<<<<< HEAD
+=======
+@app.route("/management/event/remove")
+@admin_login_required
+def removeevent():
+   return render_template("Admin/RemoveEvent.html")
+
+@app.route("/management/venue/remove")
+@admin_login_required
+def removevenue():
+   return render_template("Admin/RemoveVenue.html")
+
+
+>>>>>>> e737169 (Created function for ticket_booking)
 
 if __name__ == "__main__":
   app.run(debug=True)
