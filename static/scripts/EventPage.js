d842cfb8fefeec2806fe7afdd5f438f9275b0d36
gkvishal7 <gkvishal7@gmail.com>
2023-04-07 01:53:03 +0530

    Bug fixes

--- static/scripts/EventPage.js
+++ static/scripts/EventPage.js
@@ -2,12 +2,27 @@ var ticketIncrease = document.querySelector("#increment")
 var ticketDecrease = document.querySelector("#decrement")
 var bookButton = document.querySelector("#book-button")
 var alertMessage=document.querySelector("#alert-msg")
+
 var numberOfTickets = document.querySelector("#tickets_no")
+var remainingTickets=document.querySelector("#ticket-remaining")
+
 
 
 
 var count=0;
 ticketIncrease.addEventListener("click",()=>{
+    // var no_tickets=numberOfTickets.innerHTML
+    // no_tickets=parseInt(no_tickets)
+    // var ticket_rem=remainingTickets.innerHTML
+    // ticket_rem=parseInt(ticket_rem)
+    // console.log(no_tickets)
+    // if(ticket_rem<no_tickets){
+    //     ticketIncrease.classList.add("disabled")
+    // }else{
+    //     ticketIncrease.classList.contains("disabled")
+    //     ticketIncrease.classList.remove("disabled")
+    // }
+    
     if(count>=0){
         ticketDecrease.classList.remove("disabled")
         bookButton.classList.remove("disabled")
@@ -40,6 +55,10 @@ function getnooftickets(){
     document.getElementById("ticketcount").value=document.getElementById("tickets_no").innerHTML
     return true
 }
+
+
+
+
 // ticketIncrease.onclick = function(){
 //     count++;
 //     numberOfTickets.innerHTML="hell";
